0d07ca93be2ce8461d034162cc7b4e37ea6e1a8c
Kamil <242234@edu.p.lodz.pl>
2023-12-27 02:47:16 +0100

    Basic drag and drop in generate

--- unity/project/Assets/Scripts/DisplayStyleController.cs
+++ unity/project/Assets/Scripts/DisplayStyleController.cs
@@ -1,69 +1,69 @@
-using GaussianSplatting.Runtime;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.SceneManagement;
-using System.IO;
+//using GaussianSplatting.Runtime;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UIElements;
+//using UnityEngine.SceneManagement;
+//using System.IO;
 
-public class DisplayStyleController : MonoBehaviour
-{
-    UIDocument dropdownDoc;
-    DropdownField stylePicker;
-    DropdownField modelPicker;
-    GaussianSplatRenderer splatScene;
+//public class DisplayStyleController : MonoBehaviour
+//{
+//    UIDocument dropdownDoc;
+//    DropdownField stylePicker;
+//    DropdownField modelPicker;
+//    GaussianSplatRenderer splatScene;
 
-    public List<string> cloudList = new List<string>();
+//    public List<string> cloudList = new List<string>();
 
-    private void OnEnable()
-    {
-        dropdownDoc = GetComponent<UIDocument>();
+//    private void OnEnable()
+//    {
+//        dropdownDoc = GetComponent<UIDocument>();
 
-        stylePicker = dropdownDoc.rootVisualElement.Q<DropdownField>("DisplayStylePicker");
-        modelPicker = dropdownDoc.rootVisualElement.Q<DropdownField>("ModelPicker");
+//        stylePicker = dropdownDoc.rootVisualElement.Q<DropdownField>("DisplayStylePicker");
+//        modelPicker = dropdownDoc.rootVisualElement.Q<DropdownField>("ModelPicker");
 
-        splatScene = GameObject.Find("GaussianSplats").GetComponent<GaussianSplatRenderer>();
-
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        DirectoryInfo di = new DirectoryInfo(@".\Assets\Resources");
-        foreach (var fi in di.GetFiles())
-        {
-            if (fi.Extension == ".asset")
-            {
-                cloudList.Add(Path.GetFileNameWithoutExtension(fi.Name));
-            };
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("escape"))
-        {
-            SceneManager.LoadScene("Menu");
-        }
+//        splatScene = GameObject.Find("GaussianSplats").GetComponent<GaussianSplatRenderer>();
 
 
-        if (stylePicker.index == 1)
-        {
-            splatScene.m_RenderMode = GaussianSplatRenderer.RenderMode.DebugPoints;
-        }
-        else
-        {
-            splatScene.m_RenderMode = GaussianSplatRenderer.RenderMode.Splats;
-        }
+//    }
 
-        for (int i = 0; i < cloudList.Count; i++)
-        {
-            if (modelPicker.index == i) 
-            {
-                splatScene.m_Asset = Resources.Load(cloudList[i]) as GaussianSplatting.Runtime.GaussianSplatAsset;
-            }
-        }
-    }
-}
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        DirectoryInfo di = new DirectoryInfo(@".\Assets\Resources");
+//        foreach (var fi in di.GetFiles())
+//        {
+//            if (fi.Extension == ".asset")
+//            {
+//                cloudList.Add(Path.GetFileNameWithoutExtension(fi.Name));
+//            };
+//        }
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetKeyDown("escape"))
+//        {
+//            SceneManager.LoadScene("Menu");
+//        }
+
+
+//        if (stylePicker.index == 1)
+//        {
+//            splatScene.m_RenderMode = GaussianSplatRenderer.RenderMode.DebugPoints;
+//        }
+//        else
+//        {
+//            splatScene.m_RenderMode = GaussianSplatRenderer.RenderMode.Splats;
+//        }
+
+//        for (int i = 0; i < cloudList.Count; i++)
+//        {
+//            if (modelPicker.index == i) 
+//            {
+//                splatScene.m_Asset = Resources.Load(cloudList[i]) as GaussianSplatting.Runtime.GaussianSplatAsset;
+//            }
+//        }
+//    }
+//}
--- unity/project/Assets/UI Toolkit/Menu/Menu.cs
+++ unity/project/Assets/UI Toolkit/Menu/Menu.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UIElements;
+//using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
-{
-    UIDocument menuDoc;
-    Button startButton;
-    Button exitButton;
+//public class Menu : MonoBehaviour
+//{
+//    UIDocument menuDoc;
+//    Button startButton;
+//    Button exitButton;
 
-    private void OnEnable()
-    {
-        menuDoc = GetComponent<UIDocument>();
+//    private void OnEnable()
+//    {
+//        menuDoc = GetComponent<UIDocument>();
 
-        startButton = menuDoc.rootVisualElement.Q<Button>("StartButton");
-        exitButton = menuDoc.rootVisualElement.Q<Button>("ExitButton");
+//        startButton = menuDoc.rootVisualElement.Q<Button>("StartButton");
+//        exitButton = menuDoc.rootVisualElement.Q<Button>("ExitButton");
 
-        startButton.clicked += () => SceneManager.LoadScene("GSTestScene");
-        startButton.clicked += () => Application.Quit();
-    }
+//        startButton.clicked += () => SceneManager.LoadScene("GSTestScene");
+//        startButton.clicked += () => Application.Quit();
+//    }
 
-}
+//}
